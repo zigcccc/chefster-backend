6527df0c9de37ab6f654d583494dfde6498f7ef0
Žiga Krašovec <zkrasovec@gmail.com>
2021-10-28 12:07:23 +0200

    Added POC test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+from .models import Recipe
+
+
+class RecipeTestCase(TestCase):
+    user = None
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(username="testuser", is_active=True)
+
+    def test_creates_recipe_record_in_db(self):
+        recipe = Recipe.objects.create(
+            title="Test Recipe", description="Description of test recipe", creator=self.user
+        )
+        self.assertIsInstance(recipe, Recipe)
