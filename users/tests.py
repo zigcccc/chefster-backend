6ca427a35e624ae138344c578c283131b86ab32b
Žiga Krašovec <zkrasovec@gmail.com>
2021-10-29 12:01:35 +0200

    configured testing in VSCode, added basic user tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from recipes.models import Recipe
+from .models import User
+
+
+class UserTestCase(TestCase):
+    def test_creates_user_record_in_db(self):
+        user = User.objects.create(username="testuser")
+        self.assertIsInstance(user, User)
+
+    def test_returns_correct_recipes_count_for_a_user(self):
+        user1 = User.objects.create(username="testuser1")
+        user2 = User.objects.create(username="testuser2")
+
+        Recipe.objects.create(title="Test Recipe", description="Test Recipe Description", creator=user1)
+
+        self.assertEqual(user1.recipes_count(), 1)
+        self.assertEqual(user2.recipes_count(), 0)
